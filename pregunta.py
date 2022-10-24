719f230cfd9d464e8a3a429d59405681838cadaa
mtt-ramirez <anramirezlo@unal.edu.co>
2022-10-24 09:12:06 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,48 +7,31 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-from datetime import datetime
+
 
 def clean_data():
 
-    df = pd.read_csv('solicitudes_credito.csv', sep = ';', index_col = [0])
-    df_clean = df.copy()
-    df_clean = df.drop_duplicates(keep='last')
-    df_clean = df_clean.dropna(axis = 0, how = "any")
-    df_clean.sexo = df_clean.sexo.str.lower()
-    df_clean.tipo_de_emprendimiento = df_clean.tipo_de_emprendimiento.str.lower()
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    df_clean.idea_negocio = df_clean.idea_negocio.str.replace('-',' ')
-    df_clean.idea_negocio = df_clean.idea_negocio.str.replace('_',' ')
-    df_clean.idea_negocio = df_clean.idea_negocio.str.strip()
-    df_clean.idea_negocio = df_clean.idea_negocio.str.lower()
+    df.dropna(inplace=True)
+    
+    df["sexo"] = df["sexo"].apply(lambda x: str(x).lower().strip())
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].apply(lambda x: str(x).lower().strip())
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).lower().strip())
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).lower().strip())
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().strip())
 
-    df_clean.barrio = df_clean.barrio.str.replace('-',' ')
-    df_clean.barrio = df_clean.barrio.str.replace(' ','_')
-    df_clean.barrio = df_clean.barrio.str.lower()
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).replace("-"," ").replace("_"," ").strip())
+    df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().replace("_"," ").replace("-"," "))
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().replace("-", " ").replace("_", " ").strip())
 
-    df_clean.comuna_ciudadano = df_clean.comuna_ciudadano.astype('int')
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
 
-def fecha(data_fecha):
-    try:
-        df_clean_fecha = datetime.strptime(data_fecha, '%d/%m/%Y')
-        
-    except:
-        df_clean_fecha = datetime.strptime(data_fecha, '%Y/%m/%d')
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).strip("$").strip().replace(".00", "").replace(",", ""))
+    
+    df.drop(['Unnamed: 0'], axis=1,inplace=True)
+    
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
 
-
-    df_clean.fecha_de_beneficio = df_clean.fecha_de_beneficio.map(fecha)
-
-    df_clean.monto_del_credito = df_clean.monto_del_credito.str.replace('$', '')
-    df_clean.monto_del_credito = df_clean.monto_del_credito.str.replace(',', '')
-    df_clean.monto_del_credito = df_clean.monto_del_credito.str.strip()
-    df_clean.monto_del_credito = pd.to_numeric(df_clean.monto_del_credito)
-    df_clean.monto_del_credito = df_clean.monto_del_credito.astype('int')
-
-    df_clean.línea_credito = df_clean.línea_credito.str.replace('-', ' ')
-    df_clean.línea_credito = df_clean.línea_credito.str.replace('_', ' ')
-    df_clean.línea_credito = df_clean.línea_credito.str.strip()
-    df_clean.línea_credito = df_clean.línea_credito.str.lower()
-
-    df_clean = df_clean.drop_duplicates(keep='last')
-    print(df_clean)
+    return df
