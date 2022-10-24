ea39fac61edaa8f1bb46c614ff4ed90fba793ffa
mtt-ramirez <anramirezlo@unal.edu.co>
2022-10-24 09:01:32 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,48 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+from datetime import datetime
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv('solicitudes_credito.csv', sep = ';', index_col = [0])
+    df_clean = df.copy()
+    df_clean = df.drop_duplicates(keep='last')
+    df_clean = df_clean.dropna(axis = 0, how = "any")
+    df_clean.sexo = df_clean.sexo.str.lower()
+    df_clean.tipo_de_emprendimiento = df_clean.tipo_de_emprendimiento.str.lower()
 
-    #
-    # Inserte su código aquí
-    #
+    df_clean.idea_negocio = df_clean.idea_negocio.str.replace('-',' ')
+    df_clean.idea_negocio = df_clean.idea_negocio.str.replace('_',' ')
+    df_clean.idea_negocio = df_clean.idea_negocio.str.strip()
+    df_clean.idea_negocio = df_clean.idea_negocio.str.lower()
 
-    return df
+    df_clean.barrio = df_clean.barrio.str.replace('-',' ')
+    df_clean.barrio = df_clean.barrio.str.replace(' ','_')
+    df_clean.barrio = df_clean.barrio.str.lower()
+
+    df_clean.comuna_ciudadano = df_clean.comuna_ciudadano.astype('int')
+
+def fecha(data_fecha):
+    try:
+        df_clean_fecha = datetime.strptime(data_fecha, '%d/%m/%Y')
+        
+    except:
+        df_clean_fecha = datetime.strptime(data_fecha, '%Y/%m/%d')
+
+
+    df_clean.fecha_de_beneficio = df_clean.fecha_de_beneficio.map(fecha)
+
+    df_clean.monto_del_credito = df_clean.monto_del_credito.str.replace('$', '')
+    df_clean.monto_del_credito = df_clean.monto_del_credito.str.replace(',', '')
+    df_clean.monto_del_credito = df_clean.monto_del_credito.str.strip()
+    df_clean.monto_del_credito = pd.to_numeric(df_clean.monto_del_credito)
+    df_clean.monto_del_credito = df_clean.monto_del_credito.astype('int')
+
+    df_clean.línea_credito = df_clean.línea_credito.str.replace('-', ' ')
+    df_clean.línea_credito = df_clean.línea_credito.str.replace('_', ' ')
+    df_clean.línea_credito = df_clean.línea_credito.str.strip()
+    df_clean.línea_credito = df_clean.línea_credito.str.lower()
+
+    df_clean = df_clean.drop_duplicates(keep='last')
+    print(df_clean)
